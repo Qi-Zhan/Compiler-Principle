4197a8454310e8974526dbc592791ade1a404f02
Qi-Zhan <3190103512@zju.edu.cn>
2022-04-30 07:03:33 +0000

    complete simple array

--- test/ArrayTest2.c
+++ test/ArrayTest2.c
@@ -1,12 +1,13 @@
-
+int b[4] = {1, 2, 3, 4};
 int f(int a[], int n)
 {
+
     int num = 0;
-    int i;
-    int b[4] = {1, 2, 3, 4};
+    int i = b[3];
+    
     for (i = 0; i < n; i=i+1)
     {
-        num = num + a[i] + b[0];
+        num = num + a[i] + b[i];
         a[i] = a[i] * 2;
     }
 
